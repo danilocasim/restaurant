f0f9df0baa98eb04735a9bf23facf834f4ebff76
Danilo Casim Jr. <danilocasim174@gmail.com>
2024-12-27 17:20:18 +0800

    Add initial design for home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+import "./style.css";
+
+const content = document.querySelector("#content");
+const nav = document.querySelector("nav");
+
+// content.textContent = "HOME";
+
+// // nav.addEventListener("click", (e) => {
+// //     const target = e.target.id;
+
+// //     switch (target) {
+// //         case "home":
+// //             content.textContent = "HOME";
+// //             break;
+// //         case "about":
+// //             content.textContent = "ABOUT";
+// //             break;
+// //         case "menu":
+// //             content.textContent = "MENU";
+// //             break;
+// //     }
+// // });
